b7e2d8e8ba9aae9ec8af32674c721f549c3b556e
Victor Vicente de Carvalho <victor.v.carvalho@gmail.com>
2012-03-31 17:26:07 -0300

    Changed some parameters of the SDL/OpenGL buffer

--- src/backends/gdx-cpp-backend-linux/LinuxGraphics.cpp
+++ src/backends/gdx-cpp-backend-linux/LinuxGraphics.cpp
@@ -215,16 +215,17 @@ bool gdx_cpp::backends::nix::LinuxGraphics::setDisplayMode(int width, int height
     this->width = width;
     this->height = height;
     
-    Uint32 flags = SDL_OPENGL;
+    Uint32 flags = SDL_OPENGL | SDL_HWSURFACE;
     if (fullscreen)
         flags |= SDL_FULLSCREEN;
 
     const SDL_VideoInfo* info = SDL_GetVideoInfo();
 
-    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
-    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
-    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
     SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
+    SDL_GL_SetAttribute( SDL_GL_BUFFER_SIZE, 32 );
     SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
     
     if (!SDL_SetVideoMode(width, height, info->vfmt->BitsPerPixel, flags)) {
