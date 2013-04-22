18519837ff70a962c75a44a6840eee7fc60c0968
Victor Vicente de Carvalho <victor.v.carvalho@gmail.com>
2013-04-22 15:57:39 -0300

    Fixes on identation and warnings

--- src/gdx-cpp/graphics/g2d/Animation.cpp
+++ src/gdx-cpp/graphics/g2d/Animation.cpp
@@ -25,96 +25,103 @@
 
 using namespace gdx;
 
-Animation::Animation(float frameDuration, const std::vector< TextureRegion >& keyFrames)
-: Animation(frameDuration, keyFrames, NORMAL)
+Animation::Animation ( float frameDuration, const std::vector< TextureRegion >& keyFrames )
+    : Animation ( frameDuration, keyFrames, NORMAL )
 {
 }
 
-Animation::Animation (float frameDuration, const std::vector< TextureRegion >& keyFrames, int playType)
-: frameDuration(frameDuration),
-  animationDuration(keyFrames.size() * frameDuration),
-  keyFrames(keyFrames),
-  playMode(playType) {
+Animation::Animation ( float frameDuration, const std::vector< TextureRegion >& keyFrames, int playType )
+    : frameDuration ( frameDuration ),
+      animationDuration ( keyFrames.size() * frameDuration ),
+      keyFrames ( keyFrames ),
+      playMode ( playType )
+{
 
 }
 
 
-const TextureRegion& Animation::getKeyFrame (float stateTime, bool looping) {
-	if (looping && (playMode == NORMAL || playMode == REVERSED)) {
-		if (playMode == NORMAL) {
-			playMode = LOOP;
-		} else {
-			playMode == LOOP_REVERSED;
-		}
-	} else if (!looping && !(playMode == NORMAL || playMode == REVERSED)) {
-		if (playMode == LOOP_REVERSED) {
-			playMode = REVERSED;
-		} else {
-			playMode = LOOP;
-		}
-	}
+const TextureRegion& Animation::getKeyFrame ( float stateTime, bool looping )
+{
+    if ( looping && ( playMode == NORMAL || playMode == REVERSED ) ) {
+        if ( playMode == NORMAL ) {
+            playMode = LOOP;
+        } else {
+            playMode = LOOP_REVERSED;
+        }
+    } else if ( !looping && ! ( playMode == NORMAL || playMode == REVERSED ) ) {
+        if ( playMode == LOOP_REVERSED ) {
+            playMode = REVERSED;
+        } else {
+            playMode = LOOP;
+        }
+    }
+    return keyFrames[ getKeyFrameIndex ( stateTime )];
 }
 
-const TextureRegion& Animation::getKeyFrame(float stateTime) {
-	int frameNumber = getKeyFrameIndex(stateTime);
+const TextureRegion& Animation::getKeyFrame ( float stateTime )
+{
+    int frameNumber = getKeyFrameIndex ( stateTime );
     return keyFrames[frameNumber];
 }
 
-int Animation::getKeyFrameIndex ( float stateTime ) {
-	int frameNumber = (int)(stateTime / frameDuration);
-	if(keyFrames.size() == 1) {
-		return 0;
-	}
-	switch (playMode) {
-	case NORMAL: {
-		frameNumber = std::min(keyFrames.size() - 1, (size_t)frameNumber);
-	}
-		break;
-	case LOOP: {
-		frameNumber = frameNumber % keyFrames.size();
-	}
-		break;
-	case LOOP_PINGPONG: {
-		frameNumber = frameNumber % ((keyFrames.size() * 2) - 2);
-		if (frameNumber >= keyFrames.size()) {
-			frameNumber = keyFrames.size() - 2 - (frameNumber - keyFrames.size());
-		}
-	}
-		break;
-	case LOOP_RANDOM: {
-		frameNumber = rand() % (keyFrames.size() - 1);
-	}
-		break;
-	case REVERSED: {
-		frameNumber = std::max(keyFrames.size() - frameNumber - 1, 0u);
-	}
-		break;
-	case LOOP_REVERSED: {
-		frameNumber = frameNumber % keyFrames.size();
-		frameNumber = keyFrames.size() - frameNumber - 1;
-	}
-		break;
+int Animation::getKeyFrameIndex ( float stateTime )
+{
+    unsigned int frameNumber = ( int ) ( stateTime / frameDuration );
+    if ( keyFrames.size() == 1 ) {
+        return 0;
+    }
+    switch ( playMode ) {
+    case NORMAL: {
+        frameNumber = std::min ( keyFrames.size() - 1, ( size_t ) frameNumber );
+    }
+    break;
+    case LOOP: {
+        frameNumber = frameNumber % keyFrames.size();
+    }
+    break;
+    case LOOP_PINGPONG: {
+        frameNumber = frameNumber % ( ( keyFrames.size() * 2 ) - 2 );
+        if ( frameNumber >= keyFrames.size() ) {
+            frameNumber = keyFrames.size() - 2 - ( frameNumber - keyFrames.size() );
+        }
+    }
+    break;
+    case LOOP_RANDOM: {
+        frameNumber = rand() % ( keyFrames.size() - 1 );
+    }
+    break;
+    case REVERSED: {
+        frameNumber = std::max ( keyFrames.size() - frameNumber - 1, 0u );
+    }
+    break;
+    case LOOP_REVERSED: {
+        frameNumber = frameNumber % keyFrames.size();
+        frameNumber = keyFrames.size() - frameNumber - 1;
+    }
+    break;
 
-	default: {
-		// play normal otherwise
-		frameNumber = std::min(keyFrames.size() - 1, (size_t)frameNumber);
-	}
-		break;
-	}
+    default: {
+        // play normal otherwise
+        frameNumber = std::min ( keyFrames.size() - 1, ( size_t ) frameNumber );
+    }
+    break;
+    }
 
-	return frameNumber;
+    return frameNumber;
 }
 
-void Animation::setPlayMode(int playMode) {
-	this->playMode = playMode;
+void Animation::setPlayMode ( int playMode )
+{
+    this->playMode = playMode;
 }
 
-bool Animation::isAnimationFinished(float stateTime) const {
-	if (playMode != NORMAL && playMode != REVERSED) {
-		return false;
-	}
-	int frameNumber = (int)(stateTime / frameDuration);
-	return keyFrames.size() - 1 < frameNumber;
+bool Animation::isAnimationFinished ( float stateTime ) const
+{
+    if ( playMode != NORMAL && playMode != REVERSED ) {
+        return false;
+    }
+    unsigned int frameNumber = ( int ) ( stateTime / frameDuration );
+    return keyFrames.size() - 1 < frameNumber;
 }
 
 
--- src/gdx-cpp/graphics/g3d/loaders/wavefront/ObjLoader.hpp
+++ src/gdx-cpp/graphics/g3d/loaders/wavefront/ObjLoader.hpp
@@ -20,61 +20,63 @@
 using namespace std;
 using namespace gdx;
 
-namespace gdx {
+namespace gdx
+{
 
 class ObjLoader : public StillModelLoader
 {
 private:
-	class Group
-	{
-	friend class ObjLoader;
-	private:
-		string name;
-		string materialName;
-		vector<int> faces;
-		int numFaces;
-		bool hasNorms;
-		bool hasUVs;
-		Material* mat;
+    class Group
+    {
+        friend class ObjLoader;
+    private:
+        string name;
+        string materialName;
+        vector<int> faces;
+        int numFaces;
+        bool hasNorms;
+        bool hasUVs;
+        Material* mat;
 
-	public:
-		Group(string name) {
-			this->name = name;
-			numFaces = 0;
-			hasNorms = false;
-			hasUVs = false;
-			materialName = "default";
-			mat = new Material("");
-		}
-	};
-	
-	class MtlLoader {
-	private:
-		vector<Material*> materials;
-		//static AssetManager
-		static Texture* emptyTexture;
-	public:
-		MtlLoader();
-		void load(string fileName, FileHandle& textureDir);
-		Material* getMaterial(string name);
-	};
-	
-	vector<float> verts;
-	vector<float> norms;
-	vector<float> uvs;
-	vector<Group*> groups;
-	Group* setActiveGroup(const string& group);
-	int getIndex(int index, int size);
-	
-	void proccessVerteOrNormal(string& line, const char* templ, vector<float>& container);
-	void proccessUV(string& line, bool isFlip);
+    public:
+        Group ( string name ) {
+            this->name = name;
+            numFaces = 0;
+            hasNorms = false;
+            hasUVs = false;
+            materialName = "default";
+            mat = new Material ( "" );
+        }
+    };
+
+    class MtlLoader
+    {
+    private:
+        vector<Material*> materials;
+        //static AssetManager
+        static Texture* emptyTexture;
+    public:
+        MtlLoader();
+        void load ( string fileName, FileHandle& textureDir );
+        Material* getMaterial ( string name );
+    };
+
+    vector<float> verts;
+    vector<float> norms;
+    vector<float> uvs;
+    vector<Group*> groups;
+    Group* setActiveGroup ( const string& group );
+    int getIndex ( int index, int size );
+
+    void proccessVerteOrNormal ( string& line, const char* templ, vector<float>& container );
+    void proccessUV ( string& line, bool isFlip );
 public:
 
-	ObjLoader();
-	virtual ~ObjLoader();
-	StillModel* loadObj(const FileHandle& file, bool flipV = false);
-	StillModel* loadObj(const FileHandle& file, FileHandle& textureDir, bool flipV);
-	StillModel* load(const FileHandle& handle, ModelLoaderHints hints);
+    ObjLoader();
+    virtual ~ObjLoader();
+    StillModel* loadObj ( const FileHandle& file, bool flipV = false );
+    StillModel* loadObj ( const FileHandle& file, FileHandle& textureDir, bool flipV );
+    StillModel* load ( const FileHandle& handle, ModelLoaderHints hints );
 };
 
 }
